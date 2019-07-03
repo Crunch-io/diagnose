068b4d6314cd4d93156d01bc5adfc22229cdeb16
Robert Brewer <fumanchu@aminus.org>
2019-07-03 15:00:56 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="diagnose",
-    version="1.0.0",
+    version="1.0.1",
     author="Robert Brewer",
     author_email="dev@crunch.io",
     description="A library for instrumenting Python code at runtime.",
